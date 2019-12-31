0a0311342fcd7f32f2f2a074dd5c2e8c4c20c34f
renyuan <renyuan@360.net>
2019-12-31 14:07:18 +0000

    clear some rubbish code

--- extern.h
+++ extern.h
@@ -7,21 +7,4 @@ extern unsigned long kallsyms_lookup_name_addr;
 extern int get_kallsyms_lookup_name_function(void);
 
 
-/* sysctl */
-extern int sysctl_rto_backoff;
-extern int sysctl_rto_min;
-extern int sysctl_init_cwnd;
-extern int sysctl_loss_backoff;
-extern int sysctl_quick_start;
-extern int sysctl_init_rtt_interval;
-extern void tcpspeed_sysctl_register(void);
-extern void tcpspeed_sysctl_unregister(void);
-
-/* debug sysctl */
-extern int sysctl_debug_level;
-extern int sysctl_debug_except_port;
-extern int sysctl_debug_port;
-
-
-
 #endif
